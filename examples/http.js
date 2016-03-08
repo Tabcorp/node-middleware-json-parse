206c98e5edf2b19a46637e05f0b82d4862241cfc
Yoshua Wuyts <i@yoshuawuyts.com>
2016-03-08 13:08:45 +1100

    test: fix linter breakage

--- examples/http.js
+++ examples/http.js
@@ -4,7 +4,7 @@ const http = require('http')
 // create a server, parse json and respond if it was successful
 http.createServer(function (req, res) {
   const context = {}
-  jsonParse(context, 'body')(req, res, err => {
+  jsonParse(context, 'body')(function (req, res, err) {
     if (err) res.statusCode = 400
     const body = err || { message: 'succes!' }
     res.end(JSON.stringify(body))
